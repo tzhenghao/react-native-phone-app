96b65acb83970450abe8385bc615db8a8f1130e2
tzhenghao <tanzhao@umich.edu>
2018-01-28 15:39:10 -0600

    added blinky example

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,21 +1,36 @@
 import React from 'react';
 import { AppRegistry, Image, StyleSheet, Text, View } from 'react-native';
 
-class Greeting extends React.Component {
+
+class Blink extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {isShowingText: true};
+
+    // Toggle the state every second
+    setInterval(() => {
+      this.setState(previousState => {
+        return { isShowingText: !previousState.isShowingText };
+      });
+    }, 1000);
+  }
+
   render() {
+    let display = this.state.isShowingText ? this.props.text : ' ';
     return (
-      <Text>Hello {this.props.name}!</Text>
+      <Text>{display}</Text>
     );
   }
 }
 
-export default class LotsOfGreetings extends React.Component {
+export default class BlinkApp extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Greeting name='Rexxar' />
-        <Greeting name='Jaina' />
-        <Greeting name='Valeera' />
+        <Blink text='I love to blink' />
+        <Blink text='Yes blinking is so great' />
+        <Blink text='Why did they ever take this out of HTML' />
+        <Blink text='Look at me look at me look at me' />
       </View>
     );
   }
